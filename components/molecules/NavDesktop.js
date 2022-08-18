6f074cd701a3172b9387ca002593e984c8240a1a
taufik17 <taufikagungsantoso17@gmail.com>
2022-08-18 10:09:07 +0700

    [ADD] selesai nav

---
+++ components/molecules/NavDesktop.js
@@ -0,0 +1,112 @@
+import { BsEnvelope, BsBell } from "react-icons/bs";
+import { FiSearch } from "react-icons/fi";
+import styleHome from "../../styles/Home.module.css";
+import styleNav from "../../styles/Nav.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+function NavDesktop() {
+  return (
+    <>
+      <div className="row py-2">
+        <div className="col-3 d-flex justify-content-center">
+          <Image
+            src="/assets/img/logoblue.svg"
+            alt="Logo"
+            height={28}
+            width={28}
+          />
+          <h3 className="px-3 py-4">Angkasa</h3>
+        </div>
+        <div className="col-9">
+          <div className="row">
+            <div className="col-4">
+              <div className={`${styleHome.search} mb-2 mt-3`}>
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSearch();
+                  }}
+                >
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Where you want to go?"
+                    required
+                    onChange={(e) => setKeyword(e.target.value)}
+                  />
+                  <FiSearch className={styleHome.icon} />{" "}
+                </form>
+              </div>
+            </div>
+            <div className="col-8">
+              <nav className="navbar navbar-expand-lg py-4">
+                <div className="container-fluid">
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarTogglerDemo02"
+                    aria-controls="navbarTogglerDemo02"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div
+                    className="collapse navbar-collapse"
+                    id="navbarTogglerDemo02"
+                  >
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li className="nav-item">
+                        <Link href="/" passHref>
+                          <a
+                            className={`${styleNav.linkmenu} ${styleNav.activemenu} rm-decoration px-3`}
+                          >
+                            Find Ticket
+                          </a>
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                      <Link href="/" passHref>
+                          <a
+                            className={`${styleNav.linkmenu} rm-decoration px-3`}
+                          >
+                            My Booking
+                          </a>
+                        </Link>
+                      </li>
+                    </ul>
+                    <div className="row">
+                      <div className="col">
+                        <BsEnvelope className={`${styleNav.iconD} px-3`} />
+                        <Link href="/notification">
+                          <BsBell className={`${styleNav.iconD} px-3`} />
+                        </Link>
+                      </div>
+                    </div>
+                    <div className={styleNav.outlineBlue}>
+                      <div className={`${styleNav.cardProfile} card`}>
+                        <Image
+                          className={styleNav.imgProfile}
+                          src="/assets/img/profil.png"
+                          alt="profile"
+                          width="100%"
+                          height="100%"
+                          loading="lazy"
+                          layout="responsive"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavDesktop;
---
+++ pages/searchresultdesktop.js
@@ -0,0 +1,136 @@
+import style from "../styles/SearchResult.module.css";
+import Image from "next/image";
+import { ArrowLeftRight } from "react-bootstrap-icons";
+import { ArrowDownUp } from "react-bootstrap-icons";
+import NavDesktop from "../components/molecules/NavDesktop";
+
+function SearchResult() {
+  return (
+    <>
+    <NavDesktop />
+        <div className="col">
+          <div className={style.container}>
+            <section>
+              <div className="d-flex justify-content-between">
+                <div className="p-2 mx-3 mt-5">
+                  <Image
+                    src="/assets/img/btnback.svg"
+                    alt="Logo"
+                    height={18}
+                    width={18}
+                  />
+                </div>
+                <div className="p-2 mx-3 justify-content-end mt-5">
+                  <p className={style.text}>Monday, 20 July 20</p>
+                </div>
+              </div>
+              <div className="d-flex mx-4 justify-content-between text-white">
+                <div className="">
+                  <p className="p-0">From</p>
+                  <h5 className="p-0">Medan</h5>
+                  <p className="p-0">Indonesia</p>
+                </div>
+                <div className="d-flex ">
+                  <p className="p-0 d-flex align-items-center">
+                    <ArrowLeftRight />
+                  </p>
+                </div>
+                <div className=" text-end">
+                  <p className="p-0">To</p>
+                  <h5 className="p-0 ">Jakarta</h5>
+                  <p className="p-0  text-end">Indonesia</p>
+                </div>
+              </div>
+            </section>
+          </div>
+          <div className={style.container2}>
+            <div className="COL-12 mx-4 d-flex ">
+              <div className="col-8">
+                <p className="mt-4">Passanger</p>
+                <h5 className="p-0">2 Child 4 Adults</h5>
+              </div>
+              <div className="col-4 text-start">
+                <p className="mt-4">Class</p>
+                <h5 className="p-0 ">Economy</h5>
+              </div>
+            </div>
+          </div>
+          <div className="d-flex mx-4 mt-3 justify-content-between ">
+            <div className="">
+              <p className="p-0">5 Flight Found</p>
+            </div>
+            <div className="d-flex text-end">
+              <p className=" mx-1 ">Filter</p>
+              <div>
+                <ArrowDownUp className="" />
+              </div>
+            </div>
+          </div>
+          <div className={style.result}>
+            {[...new Array(4)].map((item, index) => (
+              <div
+                className="card col-11 mx-auto"
+                style={{
+                  borderRadius: "15px",
+                  padding: "10px",
+                  marginBottom: "10px",
+                  cursor: "pointer",
+                }}
+                key={index}
+              >
+                <div className="row ">
+                  <div className="col-3 d-flex justify-content-center align-items-center">
+                    <img
+                      src="/assets/img/logomaskapai.png"
+                      width="70px"
+                      height="70px"
+                      alt="image"
+                    />
+                  </div>
+                  <div className="col-9">
+                    <div>
+                      <section>
+                        <div className="d-flex mx-4 justify-content-between ">
+                          <div className="">
+                            <h4 className="p-0">IDN</h4>
+                            <p className="p-0">12:33</p>
+                          </div>
+                          <div className=" ">
+                            <p className="p-2">
+                              <Image
+                                src="/assets/img/flightlogo.svg"
+                                alt="Logo"
+                                width="25"
+                                height="25"
+                                style={{ marginLeft: "-15px" }}
+                              />
+                            </p>
+                          </div>
+                          <div className=" text-end">
+                            <h4 className="p-0 ">JPN</h4>
+                            <p className="p-0  text-end">15:21</p>
+                          </div>
+                        </div>
+                        <div className="d-flex mx-4 justify-content-between ">
+                          <div className="">
+                            <p className={style.fontsixe}>3 hours 11 minutes</p>
+                          </div>
+                          <div className=" text-end">
+                            <h6 className="p-0  text-end text-primary">
+                              $ 221
+                            </h6>
+                          </div>
+                        </div>
+                      </section>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+    </>
+  );
+}
+
+export default SearchResult;
