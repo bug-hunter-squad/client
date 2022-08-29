d13f27dbb38905617a205f0d0c287f31819bbeb8
taufik17 <taufikagungsantoso17@gmail.com>
2022-08-30 04:17:44 +0700

    slide swiper

--- components/molecules/Slick.js
+++ components/molecules/Slick.js
@@ -11,6 +11,15 @@ import Link from "next/link";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+// import required modules
+import { EffectCards } from "swiper";
+
 function Slick() {
   const [topDestination, setTopDestination] = useState([]);
   const [loadDestination, setLoadDestination] = useState(true);
@@ -58,7 +67,7 @@ function Slick() {
         </>
       ) : (
         <>
-          <Slider {...settings}>
+          {/* <Slider {...settings}>
             {topDestination.map((item, key) => (
               <div key={key}>
                 <div className={`${styleSlick.cardNew} card`}>
@@ -86,7 +95,7 @@ function Slick() {
                         </div>
                         <div className="col-4 text-center">
                           <Link href="/searchflight" passHref className="cursor">
-                              <FiChevronRight className={styleSlick.icon} />
+                            <FiChevronRight className={styleSlick.icon} />
                           </Link>
                         </div>
                       </div>
@@ -95,7 +104,54 @@ function Slick() {
                 </div>
               </div>
             ))}
-          </Slider>
+          </Slider> */}
+
+          <Swiper
+            effect={"cards"}
+            grabCursor={true}
+            modules={[EffectCards]}
+            className="mySwiper"
+          >
+            {topDestination.map((item, key) => (
+              <SwiperSlide style={{background: "rgb(0 0 0 / 0%) !important"}}>
+                
+                  <div className={`${styleSlick.cardNew} card`}>
+                    <Image
+                      className={styleSlick.imgNew}
+                      src="/assets/img/3.webp"
+                      alt="image"
+                      width="130"
+                      height="300"
+                      loading="lazy"
+                    />
+                    <div className="row card-img-overlay align-items-end">
+                      <div className={styleSlick.overlay}>
+                        <div className="col-8">
+                          <div className={`${styleSlick.airlines} `}>
+                            <p>15 airlines</p>
+                          </div>
+                        </div>
+                        <div className={`${styleSlick.textBottom} row`}>
+                          <div className="col-8">
+                            <div className={`${styleSlick.cardTitle} px-2`}>
+                              <h4>{item.flightOriginal},</h4>
+                              <h2>{item.flightDestination}</h2>
+                            </div>
+                          </div>
+                          <div className="col-4 text-center">
+                            <Link href="/searchflight" passHref className="cursor">
+                              <FiChevronRight className={styleSlick.icon} />
+                            </Link>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                
+              </SwiperSlide>
+            ))}
+
+          </Swiper>
         </>
       )}
     </>
